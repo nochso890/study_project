693fd8412e90bebb6b3d6d57b4c531fb97f4ad1a
nochso <nochso890@gmail.com>
2021-01-06 22:25:58 +0900

    test

---
+++ study_java/src/io/study/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package io.study.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+	}
+}
---
+++ study_java/src/io/study/variable/VarEx1.java
@@ -0,0 +1,12 @@
+package io.study.variable;
+
+public class VarEx1 {
+
+	public static void main(String[] args) {
+		int a = 1;
+		String b = "2";
+		
+		System.out.println("int : "+ a);
+		System.out.println("String : "+ b);
+	}
+}
